6652a3dadef00a8d37ebbb58bcbbdfa0e0b24493
Jay T <torrcorp@gmail.com>
2018-06-28 20:18:58 -0600

    error fixed

--- app/src/main/java/com/cnm/edu/criminalintent/CrimeLab.java
+++ app/src/main/java/com/cnm/edu/criminalintent/CrimeLab.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.UUID;
 
 public class CrimeLab {
-
   private static CrimeLab sCrimeLab;
 
   private List<Crime> mCrimes;
--- app/src/main/java/com/cnm/edu/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/cnm/edu/criminalintent/CrimeListActivity.java
@@ -7,7 +7,7 @@ public class CrimeListActivity extends SingleFragmentActivity {
 
   @Override
   protected Fragment createFragment() {
-    return new CrimeFragment();
+    return new CrimeListFragment();
   }
 
 }
