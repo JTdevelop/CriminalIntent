dad99ca3917d201aec5ca2365bd6b26bb9be4178
Jay T <torrcorp@gmail.com>
2018-06-29 08:12:29 -0600

    page 174. not able to import dependency for recycler view.

--- app/src/main/java/com/cnm/edu/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/cnm/edu/criminalintent/CrimeListFragment.java
@@ -3,6 +3,8 @@ package com.cnm.edu.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,12 @@ import java.util.List;
 public class CrimeListFragment extends Fragment {
 
   private RecyclerView mCrimeRecyclerView;
+
+  private class CrimeHolder extends RecyclerView.ViewHolder {
+    public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+      super(inflater.inflate(R.layout.list_item_crime, parent, false));
+    }
+  }
 
   private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
 
@@ -33,10 +41,6 @@ public class CrimeListFragment extends Fragment {
     return view;
   }
 
-  private class CrimeHolder extends RecyclerView.ViewHolder {
-    public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
-      super(inflater.inflate(R.layout.list_item_crime, parent, false));
-    }
-  }
+
 
 }
