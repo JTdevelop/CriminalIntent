45b65de5e0f24fbe0c9ac71233e10420dec2fa30
Jay T <torrcorp@gmail.com>
2018-06-29 12:55:17 -0600

    Chapter 8 Complete.

--- app/src/main/java/com/cnm/edu/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/cnm/edu/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import java.util.List;
 
 public class CrimeListFragment extends Fragment {
@@ -30,12 +31,12 @@ public class CrimeListFragment extends Fragment {
   private void updateUI() {
     CrimeLab crimeLab = CrimeLab.get(getActivity());
     List<Crime> crimes = crimeLab.getCrimes();
-
     mAdapter = new CrimeAdapter(crimes);
     mCrimeRecyclerView.setAdapter(mAdapter);
   }
 
-  private class CrimeHolder extends RecyclerView.ViewHolder {
+  private class CrimeHolder extends RecyclerView.ViewHolder
+    implements View.OnClickListener  {
 
     private Crime mCrime;
     private TextView mTitleTextView;
@@ -43,14 +44,23 @@ public class CrimeListFragment extends Fragment {
 
     public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
       super(inflater.inflate(R.layout.list_item_crime, parent, false));
+      itemView.setOnClickListener(this);
 
       mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
       mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
     }
 
+    @Override
+        public void onClick(View view){
+        Toast.makeText(getActivity(),
+            mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
+            .show();
+      }
+
     public void bind(Crime crime) {
       mCrime = crime;
-      mTitleTextView.setText(mCrime.getDate().toString());
+      mTitleTextView.setText(mCrime.getTitle());
+         mDateTextView.setText(mCrime.getDate().toString());
     }
   }
 
@@ -83,5 +93,9 @@ public class CrimeListFragment extends Fragment {
 
   }
 }
+
+
+
+
 
 
