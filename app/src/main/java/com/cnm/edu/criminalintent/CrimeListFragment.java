d15b893cf40c3c43a613c36f311334c80fcc93cb
Jay T <torrcorp@gmail.com>
2018-07-04 18:27:32 -0600

    Chapter 9 date not formatted.

--- app/src/main/java/com/cnm/edu/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/cnm/edu/criminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
@@ -41,6 +42,7 @@ public class CrimeListFragment extends Fragment {
     private Crime mCrime;
     private TextView mTitleTextView;
     private TextView mDateTextView;
+    private ImageView mSolvedImageView;
 
     public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
       super(inflater.inflate(R.layout.list_item_crime, parent, false));
@@ -48,6 +50,7 @@ public class CrimeListFragment extends Fragment {
 
       mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
       mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+      mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
     }
 
     @Override
@@ -60,7 +63,8 @@ public class CrimeListFragment extends Fragment {
     public void bind(Crime crime) {
       mCrime = crime;
       mTitleTextView.setText(mCrime.getTitle());
-         mDateTextView.setText(mCrime.getDate().toString());
+      mDateTextView.setText(mCrime.getDate().toString());
+      mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
     }
   }
 
